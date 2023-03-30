58b6960dc641dcc9d71ddb9db407aecf37fcf878
lola-source <99507617+lola-source@users.noreply.github.com>
2023-03-31 00:11:52 +0300

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+"""
+Create a function def island_perimeter(grid)
+that returns the perimeter of the island described in grid:
+"""
 
 
 def island_perimeter(grid):
-    width = len(grid[0])
-    height = len(grid)
-    edges = 0
-    size = 0
-
-    for i in range(height):
-        for j in range(width):
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                size += 1
-                if (j > 0 and grid[i][j - 1] == 1):
-                    edges += 1
-                if (i > 0 and grid[i - 1][j] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+    return perimeter
